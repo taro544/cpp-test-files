b7b57d2edee654e02c54731bc54e63a7dfa80399
taro544 <91344478+taro544@users.noreply.github.com>
2022-11-01 16:57:34 +0300

    Create firs.cpp

---
+++ firs.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cctype>
+using namespace std;
+
+int main(){
+string myString;
+cout << "WRITE SOMETHING" <<endl;
+getline(cin ,myString);
+int check;
+   for (int i = 0; i < myString.size(); i++) {
+      check = isdigit(myString[i]);
+      if (check) {
+         if (isdigit(myString[i-1])){
+            cout << myString[i];
+         }
+         else {
+            cout << "\n" << myString[i];
+         }
+      }
+      else {
+         if(isdigit(myString[i-1])){
+            cout << "\n" << myString[i];
+         }
+         else {
+            cout << myString[i];
+         }
+      }
+   }
+}
